6a0093c82174604fa3b62a3873ceb94f69299fa7
TASK FORCE 141 <ozhan-yildirim@hotmail.com>
2023-07-03 16:53:09 +0300

    v2

---
+++ carv1/Client/Model/Cars.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using static System.Net.WebRequestMethods;
+
+namespace carv1.Client.Model
+
+{
+    public class Cars
+    {
+     public string Name { get; set; }
+     public string ImageUrl { get; set; }
+     public string Desc { get; set; }
+
+
+
+    }
+
+
+  
+}
